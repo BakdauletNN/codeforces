95cd0b134a16eec5b99c4d9b3bc51ae244145960
Bakdaulet Rysbekov <sspacee007@gmail.com>
2025-05-11 15:57:31 +0600

    test_file

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main() {
+    int array[6];
+    int idx = 3; 
+    for (int i = 0; i < 6; i++) {
+        std::cin >> array[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        if(array[i] == array[idx]){
+            std::cout<<"YES";
+            break;
+        idx ++;
+        }
+    
+    else{
+        std::cout <<"NO";
+    }
+
+    }
+    return 0;
+}
